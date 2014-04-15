c31917184e1555a2dee811e886f488af76113a13
Benjamin Rau <rau@codearts.at>
2014-04-15 16:12:43 +0200

    [FEATURE] Turn on overlays if no EXTCONF exists
    Since these checkboxes are new the overlay functionality would be disabled when when a user updates to 1.3 because there is no EXTCONF.
    
    Checking for isset enables the feature if a user comes from earlier versions.
    
    For fresh installation of EXT:view the default values are written to EXTCONF automatically so checking for ISSET is a good way to keep the feature enabled for update but have it disabled by default for fresh installations.

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,23 +8,23 @@ $script = t3lib_div::getIndpEnv('SCRIPT_FILENAME');
 $version = TYPO3_version;
 if ('sysext/install/Start/Install.php' !== substr($script, -32)) {
 	if ($version{0} >= 6) {
-		if (TRUE === (boolean) $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['view']['setup']['overlays']) {
+		if (TRUE === (boolean) $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['view']['setup']['overlays'] || FALSE === isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['view']['setup']['overlays'])) {
 			$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['Tx_Fluid_View_TemplateView'] =
 				array('className' => 'Tx_View_Override_View_ExtendedTemplateView');
 			$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Fluid\\View\\TemplateView'] =
 				array('className' => 'FluidTYPO3\\View\\Override\\View\\ExtendedTemplateView');
 		}
-		if (TRUE === (boolean) $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['view']['setup']['viewhelperarguments']) {
+		if (TRUE === (boolean) $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['view']['setup']['viewhelperarguments']  || FALSE === isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['view']['setup']['viewhelperarguments'])) {
 			$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['Tx_Fluid_Core_Parser_SyntaxTree_ViewHelperNode'] =
 				array('className' => 'Tx_View_Override_Parser_SyntaxTree_ExtendedViewHelperNode');
 			$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Fluid\\Core\\Parser\\SyntaxTree\\ViewHelperNode'] =
 				array('className' => 'FluidTYPO3\\View\\Override\\Parser\\SyntaxTree\\ExtendedViewHelperNode');
 		}
 	} else {
-		if (TRUE === (boolean) $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['view']['setup']['overlays']) {
+		if (TRUE === (boolean) $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['view']['setup']['overlays']  || FALSE === isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['view']['setup']['overlays'])) {
 			class_alias('Tx_View_Override_View_ExtendedTemplateView', 'ux_Tx_Fluid_View_TemplateView');
 		}
-		if (TRUE === (boolean) $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['view']['setup']['viewhelperarguments']) {
+		if (TRUE === (boolean) $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['view']['setup']['viewhelperarguments']  || FALSE === isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['view']['setup']['viewhelperarguments'])) {
 			class_alias('Tx_View_Override_Parser_SyntaxTree_ExtendedViewHelperNode', 'ux_Tx_Fluid_Core_Parser_SyntaxTree_ViewHelperNode');
 		}
 	}
